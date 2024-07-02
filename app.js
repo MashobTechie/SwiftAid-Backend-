22aa06c52bde289ea797c52c64134028252de845
Sheriffdeen Makinde <sheriffdeenmakinde@gmail.com>
2024-07-02 10:26:43 +0100

    generateInitialsImageUrl completed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const morgan = require ("morgan")
-const cors = require("cors")
-const errorHandler = require ("./midddlewares/error")
-const authRoutes = require("./routes/authRoute")
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const errorHandler = require("./midddlewares/error");
+const authRoutes = require("./routes/authRoute");
 
 const app = express();
 
@@ -12,22 +12,22 @@ app.use(morgan("dev"));
 app.use(cors("*"));
 
 app.get("/api/v1", (req, res) => {
-    res.status(200).json({
-      status: "success",
-      message: "Welcome to SwiftAid",
-    });
+  res.status(200).json({
+    status: "success",
+    message: "Welcome to SwiftAid",
   });
+});
 
 
-  app.use('/api/v1/auth', authRoutes);
+app.use("/api/v1/auth", authRoutes);
 
-  app.all("*", (req, res) => {
-    res.status(404).json({
-      status: "fail",
-      message: `Can't find ${req.originalUrl} with method ${req.method} on this server. Route not defined`,
-    });
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} with method ${req.method} on this server. Route not defined`,
   });
+});
 
-  // Calling our error handler
+// Calling our error handler
 app.use(errorHandler);
-  module.exports = app;
+module.exports = app;
--- utils/generateInitialsImageUrl.js
+++ utils/generateInitialsImageUrl.js
@@ -1,11 +1,10 @@
 const generateInitialsImageUrl = (firstname, lastname) => {
-    const initials = `${firstname[0]}${lastname[0]}`.toUpperCase();
-    const color = "FF0000"; // Red background color
-    const textColor = "FFFFFF"; // White text color
-    const size = 128;
-    const url = `https://via.placeholder.com/${size}/${color}/${textColor}?text=${initials}`;
-    return url;
-  };
-  
-  module.exports = generateInitialsImageUrl;
-  
+  const initials = `${firstname[0]}${lastname[0]}`.toUpperCase();
+  const color = "FF0000"; // Red background color
+  const textColor = "FFFFFF"; // White text color
+  const size = 128;
+  const url = `https://via.placeholder.com/${size}/${color}/${textColor}?text=${initials}`;
+  return url;
+};
+
+module.exports = generateInitialsImageUrl;
